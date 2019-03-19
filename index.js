b7b715ec9d1b26e5739a5b49bd540498c134ebe8
aayush6194 <aayush6194@gmail.com>
2019-03-18 21:58:36 -0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require("body-parser"); //post request
-const urlencodedParser = bodyParser.urlencoded({extended: false});
-const multer = require('multer');
-const port =  process.env.PORT || 3007;
-const app = express();
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
+// const bodyParser = require("body-parser"); //post request
+// const urlencodedParser = bodyParser.urlencoded({extended: false});
+// const multer = require('multer');
+// const port =  process.env.PORT || 3007;
+// const app = express();
+// const GridFsStorage = require('multer-gridfs-storage');
+// const Grid = require('gridfs-stream');
 // const db = require("./db");
 
 // db.connect("", {name: String, email: String,message: String
@@ -27,14 +27,14 @@ const Grid = require('gridfs-stream');
 // });
 
 //const upload = multer({ storage });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // app.post('/upload', upload.single('file'),  (req, res) => {
 //   res.redirect('https://www.aayushh.com/Message/');
