f0dc2d89a91a6a8749fbcb3b6a27814e12d4346c
aayush6194 <aayush6194@gmail.com>
2019-03-18 22:12:18 -0500

    done

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 // const bodyParser = require("body-parser"); //post request
 // const urlencodedParser = bodyParser.urlencoded({extended: false});
 // const multer = require('multer');
-// const port =  process.env.PORT || 3007;
-// const app = express();
+ const port =  process.env.PORT || 3007;
+ const app = express();
 // const GridFsStorage = require('multer-gridfs-storage');
 // const Grid = require('gridfs-stream');
-// const db = require("./db");
-
-// db.connect("", {name: String, email: String,message: String
-//               //,  img: { data: Buffer, contentType: String }
-//             });
-
+// //const db = require("./db");
+//
+// // db.connect("", {name: String, email: String,message: String
+// //               //,  img: { data: Buffer, contentType: String }
+// //             });
+//
 // const storage = new GridFsStorage({
 //   url: "mongodb://aayush6194:poop12@ds041571.mlab.com:41571/portfolio",
 //   file: (req, file) => {
@@ -25,17 +25,17 @@ const express = require('express');
 //     });
 //   }
 // });
-
-//const upload = multer({ storage });
+//
+// const upload = multer({ storage });
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
-
+//
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
-
+//
 // app.post('/upload', upload.single('file'),  (req, res) => {
 //   res.redirect('https://www.aayushh.com/Message/');
 // });
